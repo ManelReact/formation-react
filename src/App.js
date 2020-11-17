f18cd906f9671c4f18c077df55a526dfb01eeac9
Manel <74536995+ManelReact@users.noreply.github.com>
2020-11-17 14:39:34 +0100

    4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './kobo.png';
 import Login from './Login';
 import Login2 from './Login2.js';
+import Page2 from './Page2.js';
 import Menu from './Menu';
 import Recherche from './Recherche';
 import './App.css';
 import { BrowserRouter as Router, Redirect, Route, Switch } from
 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
+  
     <div className="App">
+      <Router>
+        <Switch>
+        <Route path='/Menu' component={Menu}/>
+        <Route path='/Page2' component={Page2}/>
+        </Switch>
+      <nav>
+      <ul>
+      <li><Link to='/' >Page1</Link></li>
+        <li><Link to='/Page2' >Page2</Link></li>
+      </ul>
+    </nav>
+    </Router>
       <Login />
       <Login2 />
       <Recherche />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
       </header>
     </div>
+  
   );
 }
 
---
+++ src/Page2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page2 = () => (
+  <div>
+    <h1>Page d'accueil</h1>
+  </div>
+)
+
+export default Page2
