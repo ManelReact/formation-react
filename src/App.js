dc07e72817cfff0223c1321c4efe2a7382018cf6
Manel <74536995+ManelReact@users.noreply.github.com>
2020-11-17 16:19:51 +0100

    5

--- src/App.js
+++ src/App.js
@@ -6,33 +6,30 @@ import Menu from './Menu';
 import Recherche from './Recherche';
 import './App.css';
 import { BrowserRouter as Router, Redirect, Route, Switch } from
-'react-router-dom';
+  'react-router-dom';
 import { Link } from 'react-router-dom'
 
 function App() {
   return (
-  
+
     <div className="App">
       <Router>
         <Switch>
-        <Route path='/Menu' component={Menu}/>
-        <Route path='/Page2' component={Page2}/>
+          <Route path='/Menu' component={Menu} />
+          <Route path='/Page2' component={Page2} />
         </Switch>
-      <nav>
-      <ul>
-      <li><Link to='/' >Page1</Link></li>
-        <li><Link to='/Page2' >Page2</Link></li>
-      </ul>
-    </nav>
-    </Router>
+        <nav>
+          <ul>
+            <li><Link to='/' >Page1</Link></li>
+            <li><Link to='/Page2' >Page2</Link></li>
+          </ul>
+        </nav>
+      </Router>
       <Login />
       <Login2 />
       <Recherche />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
     </div>
-  
+
   );
 }
 
--- src/Login2.js
+++ src/Login2.js
@@ -1,68 +1,72 @@
-/*import React from 'react';
+
+import React, { useState } from 'react';
 
 function Login2() {
 
  
-  let [ username, password] = useState('');
-  
-  function connect(a,b) {
-    if(a=='1' && b=='2')
+  const [username, setUsername] = useState(0);
+  const [password, setPassword] = useState(0);
+
+  function connect(username,password) {
+    if(username=='toto' && password=='toto')
     {alert('ok');}
     else
     {alert ('non');}
   }
-  function handleChange(e) {
-    console.log(e.target.value);
-  }
+
+ 
   return (
     <div className="Login2">
-    Utilisateur <input type="text" onChange={e => e.username(e.target.value)} />
-    Password <input type="text" onChange={e=> e.password(e.target.value)} />
-    <button onClick={ connect.bind(this,e.username,e.password) }>
+    Utilisateur <input type="text" onChange={e =>setUsername(e.target.value)} />
+    Password <input type="text" onChange={e =>setPassword(e.target.value)} />
+    <button onClick={ connect.bind(this,username,password) }>
       connexion
     </button>
     </div>  
   );
 }
 
-export default Login2;*/
-
-import React, { Component } from 'react';
-
-
-class Login2 extends Component{
-
-    constructor(props){
-        super(props);
-        //this.connect=this.connect.bind(this)
-        this.state = {
-          username: '',
-          password: '',
-        }
-    }
-
-    connect() {
-      if(this.state.username=='1' && this.state.password=='2')
-      alert('ok');
-      else
-      alert ('non');
-    }
-
-    log() {
-      console.log(this.state.username);
-      console.log(this.state.password);
-   }
-
-    render(){
-      return(
-       <div className="Login2">
-    Utilisateur <input type="text" onChangeText={(text) => this.setState({username:text})} />
-    Password <input type="text" onChangeText={(text) => this.setState({password:text})} />
-    <button onClick={ this.connect.bind(this)}>
-      connexion
-    </button>
-    </div>  
-      )
-   }
-}
 export default Login2;
+/*
+import React, { Component } from 'react';
+import Page2 from './Page2.js';
+import { BrowserRouter as Router, Redirect, Route, Switch } from
+  'react-router-dom';
+import { Link } from 'react-router-dom'
+
+class Login2 extends Component {
+
+  constructor(props) {
+    super(props);
+    //this.connect=this.connect.bind(this)
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  connect() {
+    if (this.state.username == '1' && this.state.password == '2')
+      alert('ok');
+    else
+      alert('not ok');
+  }
+
+  log() {
+    console.log(this.state.username);
+    console.log(this.state.password);
+  }
+
+  render() {
+    return (
+      <div className="Login2">
+        Utilisateur <input type="text" onChangeText={(text) => this.setState({ username: text })} />
+    Password <input type="text" onChangeText={(text) => this.setState({ password: text })} />
+        <button onClick={this.connect.bind(this)}>
+          connexion
+    </button>
+      </div>
+    )
+  }
+}
+export default Login2;*/
